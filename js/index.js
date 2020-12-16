4f9bee3c35cfa13cda179860e52b3fba83264466
qiao <iosqiao@qq.com>
2020-12-16 14:27:57 +0800

    Site updated: 2020-12-16 14:27:57

--- js/index.js
+++ js/index.js
@@ -59,7 +59,7 @@
 
         $backToTop.click(function () {
             console.log('click');
-            $('html,body').animate({ scrollTop: 0 });
+            $('html,body').animate({scrollTop: 0});
         });
     };
 
